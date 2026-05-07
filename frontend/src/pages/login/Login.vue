6aae919b09e997db6ab03772959a84470b2811e2
heejudy <moheeju1223@gmail.com>
2026-05-07 12:59:44 +0900

    젠킨스 테스트 6

--- frontend/src/pages/login/Login.vue
+++ frontend/src/pages/login/Login.vue
@@ -9,7 +9,7 @@
         />
 
         <h1 class="text-3xl font-black text-[green] tracking-tight">
-          두산책
+          brainwalk
         </h1>
         <p class="text-gray-500 text-sm font-medium">
           기억을 잇고, 건강을 걷다
